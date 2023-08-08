8425761b381ac550cc660cd23152b22061a6719b
shailesh <sahushailesh9@gmail.com>
2023-08-09 00:18:22 +0530

    updateteh develop

---
+++ src/Footer.js
@@ -0,0 +1,8 @@
+function Footer() {
+    return ( <footer>
+      <small>@ 2021 Shailesh Development Team. All rights reserved</small>
+    </footer>);
+  }
+
+  
+export default Footer;
---
+++ src/MainContent.js
@@ -0,0 +1,14 @@
+function MainContent() {
+    return (<div>
+     <h3>Why you want to learn react?</h3>
+     <ol>
+       <li>To get into good or get more job opportunity</li>
+       <li>Handle the FE</li>
+       <li>Widen the Learning curve</li>
+       <li>Increase my salary</li>
+       <li>Able to lead.</li>
+     </ol>
+    </div>);
+}
+
+export default MainContent;
